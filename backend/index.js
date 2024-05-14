4d4dbbab8788ba7941cb061a895713be0822b57b
Pyry Lahtinen <pyry@pyry.info>
2024-05-14 10:37:31 +0300

    moved database from frontend to backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
+require('dotenv').config()
 const express = require('express')
+const sql = require('./database')
 
 const app = express()
 
 app.get('/api', (req, res) => {
   res.send('hello world')
+})
+
+app.get('/api/users', async (req, res) => {
+  // FIXME: remove this route later during the development
+  const users = await sql`
+    SELECT user_name FROM users;
+  `
+  res.json(users)
 })
 
 app.listen(8080, () => {
