4093aea2c12ef78e7842c543d5eca704eced3618
Roberto Martinez <robomarti@outlook.com>
2024-05-15 13:46:05 +0300

    added online test database

--- backend/database.js
+++ backend/database.js
@@ -1,12 +1,14 @@
 const postgres = require('postgres')
 
-const sql = postgres({
-  host: process.env.POSTGRES_IP,             // Postgres ip address[s] or domain name[s]
-  port: 5432,                                // Postgres server port[s]
-  database: process.env.POSTGRES_DB_NAME,    // Name of database to connect to
-  username: process.env.POSTGRES_USERNAME,   // Username of database user
-  password: process.env.POSTGRES_PASSWORD,   // Password of database user
-})
+//const sql = postgres('postgres://username:password@host:port/database', {
+//  host: process.env.POSTGRES_IP,             // Postgres ip address[s] or domain name[s]
+//  port: 5432,                                // Postgres server port[s]
+//  database: process.env.POSTGRES_DB_NAME,    // Name of database to connect to
+//  username: process.env.POSTGRES_USERNAME,   // Username of database user
+//  password: process.env.POSTGRES_PASSWORD,   // Password of database user
+//})
+
+const sql = postgres(process.env.BACKEND_POSTGRES_URL)
 
 const insertUser = async (username, passwordHash) => {
   await sql`
