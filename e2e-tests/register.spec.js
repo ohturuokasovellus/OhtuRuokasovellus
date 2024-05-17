ac9ff59b9e4a45dd4d2cc8bf41accb43be0c3482
meri saynatkari <meri.saynatkari@gmail.com>
2024-05-17 15:14:34 +0300

    add e2e test skeletons for registration form

--- e2e-tests/register.spec.js
+++ e2e-tests/register.spec.js
@@ -5,3 +5,35 @@ test('user can be redirected to login page if already registered', async ({ page
     await page.getByRole('link', { name: 'login' }).click();
     await expect(page).toHaveURL(/\/login$/)
 });
+
+test('user can register with correct details', async ({ page }) => {
+    await page.goto('/register')
+});
+
+test('user cannot register with a duplicate username', async ({ page }) => {
+    await page.goto('/register')
+});
+
+test('user cannot register without a username', async ({ page }) => {
+    await page.goto('/register')
+});
+
+test('user cannot register with a duplicate email', async ({ page }) => {
+    await page.goto('/register')
+});
+
+test('user cannot register with an invalid email', async ({ page }) => {
+    await page.goto('/register')
+});
+
+test('user cannot register without an email', async ({ page }) => {
+    await page.goto('/register')
+});
+
+test('user cannot register with an invalid password', async ({ page }) => {
+    await page.goto('/register')
+});
+
+test('user cannot register if the passwords do not match', async ({ page }) => {
+    await page.goto('/register')
+});
