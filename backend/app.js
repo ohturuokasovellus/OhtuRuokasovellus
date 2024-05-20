f9c42eda88d6958620a6b16a3a3f215251ff56ae
Pyry Lahtinen <pyry@pyry.info>
2024-05-20 09:09:00 +0300

    removed unnecessary api endpoints

--- backend/app.js
+++ backend/app.js
@@ -12,20 +12,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/api', (req, res) => {
-  res.send('hello world')
-})
-
 app.use(registerRouter)
 app.use(LoginRouter)
-
-app.get('/api/users', async (req, res) => {
-  // FIXME: remove this route later during the development
-  const users = await sql`
-    SELECT user_name FROM users;
-  `
-  res.json(users)
-})
 
 const webBuildPath = path.join(__dirname, '..', 'web-build')
 if (fs.existsSync(webBuildPath)) {
