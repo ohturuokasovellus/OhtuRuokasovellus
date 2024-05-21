b2b8a59cd25ca9a12c07689ff1524f6e4155b0d4
Pyry Lahtinen <pyry@pyry.info>
2024-05-21 10:49:43 +0300

    fixing lint errors

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,27 +1,28 @@
 import { useEffect } from "react";
-import { Text, View } from "react-native"
-import { useNavigate } from '../Router'
-import { styles } from '../styling/styles'
+import { Text, View } from "react-native";
+import { useNavigate } from '../Router';
+import { styles } from '../styling/styles';
 
 const HomePage = (props) => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!props.user) {
-      navigate('/login')
-    }
-  }, [])
-
-  if (!props.user) {
-    navigate('/login')
-  }
-  return (
-      <View>
-        {props.user && 
-          <Text style={styles.welcomeText}>Welcome, {props.user.username}
-          </Text>
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!props.user) {
+            navigate('/login');
         }
-      </View>
-  )
-}
+    }, []);
 
-export default HomePage
+    if (!props.user) {
+        navigate('/login');
+    }
+
+    return (
+        <View>
+            {props.user && 
+                <Text style={styles.welcomeText}>Welcome, {props.user.username}
+                </Text>
+            }
+        </View>
+    );
+};
+
+export default HomePage;
