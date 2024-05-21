3110ca5d0c3563d430ee1b3405e894105ca58046
Roberto Martinez <robomarti@outlook.com>
2024-05-21 16:45:33 +0300

    updated schema

--- schema.sql
+++ schema.sql
@@ -2,17 +2,17 @@ DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS restaurants CASCADE;
 DROP TABLE IF EXISTS meals CASCADE;
 
+CREATE TABLE restaurants (
+    restaurant_id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
+);
+
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
-    restaurant_id INT REFERENCES restaurants ON DELETE NULL
-);
-
-CREATE TABLE restaurants (
-    restaurant_id SERIAL PRIMARY KEY,
-    name TEXT UNIQUE NOT NULL
+    restaurant_id INT REFERENCES restaurants DEFAULT NULL
 );
 
 CREATE TABLE meals (
