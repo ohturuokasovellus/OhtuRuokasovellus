31145422cf9e16d8c767e0abdac081ee05c9589f
meri saynatkari <meri.saynatkari@gmail.com>
2024-05-21 15:56:22 +0300

    add db table for restaurants

--- schema.sql
+++ schema.sql
@@ -1,8 +1,14 @@
-DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS users, restaurants CASCADE;
 
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL
+    email TEXT UNIQUE NOT NULL,
+    restaurant_id INTEGER REFERENCES restaurants ON DELETE SET NULL
+);
+
+CREATE TABLE restaurants (
+    restaurant_id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
 );
