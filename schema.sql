9341f2a492bf009624fd5baf5beb77aaec4a813d
meri saynatkari <meri.saynatkari@gmail.com>
2024-05-22 11:54:37 +0300

    update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,9 @@
-DROP TABLE IF EXISTS users, restaurants CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS restaurants CASCADE;
 
 CREATE TABLE restaurants (
     restaurant_id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
+    name TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE users (
@@ -10,5 +11,5 @@ CREATE TABLE users (
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
-    restaurant_id INTEGER REFERENCES restaurants ON DELETE SET NULL
+    restaurant_id INTEGER REFERENCES restaurants DEFAULT NULL
 );
