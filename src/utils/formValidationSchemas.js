e0d6bac2fd64f196711287021271f285aa883f37
meri saynatkari <meri.saynatkari@gmail.com>
2024-05-22 09:32:55 +0300

    add validation schema for restaurant registration

--- src/utils/formValidationSchemas.js
+++ src/utils/formValidationSchemas.js
@@ -5,17 +5,17 @@ import * as yup from 'yup';
 // https://github.com/jquense/yup#stringemailmessage-string--function-schema
 yup.addMethod(yup.string, 'email', function validateEmail(message) {
     return this.matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, {
-      message,
-      name: 'email',
-      excludeEmptyString: true,
+        message,
+        name: 'email',
+        excludeEmptyString: true,
     });
-  });
+});
 
 const registrationValidationSchema = yup.object().shape({
     username: yup.string()
-    .min(3, 'username must be at least 3 characters')
-    .max(32, 'username cannot exceed 32 characters')
-    .required('username is required'),
+        .min(3, 'username must be at least 3 characters')
+        .max(32, 'username cannot exceed 32 characters')
+        .required('username is required'),
     email: yup.string().email('invalid email').required('email is required'),
     password: yup
         .string()
@@ -24,7 +24,10 @@ const registrationValidationSchema = yup.object().shape({
         .matches(/[a-z]/, 'password must contain at least one lowercase letter')
         .matches(/[A-Z]/, 'password must contain at least one uppercase letter')
         .matches(/\d/, 'password must contain at least one number')
-        .matches(/[@$!%&€\-_:#+]/, 'password must contain at least one special character')
+        .matches(
+            /[@$!%&€\-_:#+]/,
+            'password must contain at least one special character'
+        )
         .required('password is required'),
     confirmPassword: yup
         .string()
@@ -32,6 +35,14 @@ const registrationValidationSchema = yup.object().shape({
         .required('password confirmation is required'),
 });
 
+const restaurantValidationSchema = registrationValidationSchema.shape({
+    name: yup.string()
+        .min(3, 'restaurant name must be at least 3 characters')
+        .max(32, 'restaurant name cannot exceed 32 characters')
+        .required('restaurant name is required'),
+});
+
 export {
-    registrationValidationSchema
+    registrationValidationSchema,
+    restaurantValidationSchema
 };
