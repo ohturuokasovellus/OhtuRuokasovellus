598a0e98420fee73f4f21a6db13db9cf0cc20de3
Pyry Lahtinen <pyry@pyry.info>
2024-05-24 11:35:38 +0300

    add broken meal creation e2e test

--- e2e-tests/mealCreation.spec.ts
+++ e2e-tests/mealCreation.spec.ts
@@ -1,6 +1,7 @@
 import { sql } from '../backend/database';
 import { test, expect } from '@playwright/test';
 import { hash } from '../backend/services/hash';
+import path from 'node:path';
 
 test.describe('meal creation page', () => {
     test.beforeEach(async ({ page }) => {
@@ -19,4 +20,13 @@ test.describe('meal creation page', () => {
         await expect(page).toHaveURL('/create-meal');
         await expect(page.locator('#root')).toContainText('Image of the meal is required');
     });
+
+    // test('meal image works', async ({ page}) => {
+    //     await page.fill('input[placeholder="Aterian nimi"]', 'ruoka');
+    //     await page.click('text=Valitse kuva laitteelta');
+    //     const filePath = path.join(__dirname, 'assets', 'image.png');
+    //     await page.locator('input[type="file"]').setInputFiles(filePath);
+    //     await page.keyboard.press('Escape');
+    //     await expect(page.getByRole('img')).toBeVisible();
+    // });
 });
