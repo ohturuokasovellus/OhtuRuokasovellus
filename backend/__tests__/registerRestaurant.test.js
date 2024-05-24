7fd8ad592fec8f7011c3de20caea72a7b79ea5b1
Roberto Martinez <robomarti@outlook.com>
2024-05-24 13:39:30 +0300

    added tests for registering restaurant

---
+++ backend/__tests__/registerRestaurant.test.js
@@ -0,0 +1,41 @@
+const request = require('supertest');
+const app = require('../app');
+// eslint-disable-next-line jest/no-mocks-import
+const postgresMock = require('../__mocks__/postgres');
+
+describe('register restaurant api', () => {
+    afterEach(() => {
+        postgresMock.clearDatabase();
+    });
+
+    // eslint-disable-next-line jest/expect-expect
+    test('register fails with missing username', async () => {
+        await request(app)
+            .post('/api/register-restaurant')
+            .send({ password: 'Testi-123', email: 'johndoe@example.com' })
+            .set('Content-Type', 'application/json')
+            .expect(400)
+            .expect({ errorMessage: 'invalid username, password or email' });
+    });
+
+    test('registered user is saved to database', async () => {
+        postgresMock.setSqlResults([
+            [{ exists: false }],    // check if username already exists
+            [{ exists: false }],    // check if email already exists
+            null,                   // user is inserted to db, no return
+        ]);
+
+        await request(app)
+            .post('/api/register-restaurant')
+            .send({
+                username: 'tester',
+                password: 'Testi-123',
+                email: 'johndoe@example.com',
+                restaurantName: 'testaurant',
+            })
+            .set('Content-Type', 'application/json')
+            .expect(200);
+
+        expect(postgresMock.runSqlCommands().length).toBe(3);
+    });
+});
