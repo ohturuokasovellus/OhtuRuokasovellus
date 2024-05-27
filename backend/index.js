b915588ac7ca1a3df5e5df9cdd63a1e6f70ea5f1
Pyry Lahtinen <pyry@pyry.info>
2024-05-27 14:15:36 +0300

    added database connection

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,26 @@
 // const app = require('./app');
 
+require('dotenv').config();
 const express = require('express');
+const postgres = require('postgres');
 const app = express();
 
-app.get('/', (req, res) => {
-    const url = 'https://github.com/ohturuokasovellus/OhtuRuokasovellus';
-    res.send(`<h1>Ruokasovellus</h1><a href="${url}">GitHub</a>`);
+const sql = postgres(process.env.BACKEND_POSTGRES_URL);
+
+app.get('/', async (req, res) => {
+    let dbtest
+    try {
+        dbtest = (await sql`SELECT 1 AS value`).at(0).value;
+    } catch (err) {
+        console.error(err);
+        dbtest = `failed: ${err.message}`;
+    }
+
+    res.send(`
+    <h1>Ruokasovellus</h1>
+    <a href="https://github.com/ohturuokasovellus/OhtuRuokasovellus">GitHub</a>
+    <p>Database test: ${dbtest}</p>
+    `);
 });
 
 app.listen(8080, () => {
