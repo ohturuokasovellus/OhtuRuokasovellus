6109fb47d2f007201b22d7fb4f47afe74b6015d9
Pyry Lahtinen <pyry@pyry.info>
2024-05-28 10:17:54 +0300

    replace stub server with the real one

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,4 @@
-// const app = require('./app');
-
-require('dotenv').config();
-const express = require('express');
-const postgres = require('postgres');
-const app = express();
-
-const sql = postgres(process.env.BACKEND_POSTGRES_URL);
-
-app.get('/', async (req, res) => {
-    let dbtest
-    try {
-        dbtest = (await sql`SELECT 1 AS value`).at(0).value;
-    } catch (err) {
-        console.error(err);
-        dbtest = `failed: ${err.message}`;
-    }
-
-    res.send(`
-    <h1>Ruokasovellus</h1>
-    <a href="https://github.com/ohturuokasovellus/OhtuRuokasovellus">GitHub</a>
-    <p>Database test: ${dbtest}</p>
-    `);
-});
+const app = require('./app');
 
 app.listen(8080, () => {
     console.log('listening to 8080...');
