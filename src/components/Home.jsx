a7f6f213055484dbe3a17a9a5fa9f9de76ca5a10
meri saynatkari <meri.saynatkari@gmail.com>
2024-05-31 15:49:31 +0300

    update home page layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react';
-import { Text, View, Pressable } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
+
 import { useNavigate } from '../Router';
-import { styles } from '../styles/styles';
+
+import createStyles from '../styles/layout';
+import { Button } from './ui/Buttons';
 
 const Home = (props) => {
     const navigate = useNavigate();
@@ -20,32 +23,39 @@ const Home = (props) => {
     const restaurantId = props.user.restaurantId;
     const isRestaurantUser = restaurantId !== null;
 
+    const styles = createStyles();
+
     return (
-        <View>
-            <Text style={styles.welcomeText}>
+        <ScrollView style={styles.background}>
+            <View style={styles.container}>
+                <Text style={styles.h1}>Home</Text>
+                <Text style={styles.body}>
                 Welcome, {username}
-            </Text>
-            {isRestaurantUser ? (
-                <>
-                    <Text style={styles.welcomeText}>
+                </Text>
+                {isRestaurantUser ? (
+                    <>
+                        <Text style={styles.body}>
                         You are logged in as a restaurant user.
-                    </Text>
-                    <Pressable style={styles.button} title='Add user' onPress={
-                        () => navigate('/add-users')
-                    }>
-                        <Text style={styles.buttonText}>add user</Text>
-                    </Pressable>
-                    <Pressable style={styles.button} title='restaurant page'
-                        onPress={
-                            () => navigate(
-                                `/restaurant/${restaurantId}`
-                            )
-                        }>
-                        <Text style={styles.buttonText}>restaurant page</Text>
-                    </Pressable>
-                </>
-            ) : null}
-        </View>
+                        </Text>
+                        <Button
+                            styles={styles}
+                            onPress={() => navigate('/add-users')}
+                            text='add users'
+                            id='add-users-button'
+                        />
+                        <Button
+                            styles={styles}
+                            onPress={
+                                () => navigate(`/restaurant/${restaurantId}`)
+                            }
+                            text='restaurant page'
+                            id='restaurant-page-button'
+                        />
+                    </>
+                ) : null}
+            </View>
+        </ScrollView>
+    
     );
 };
 
