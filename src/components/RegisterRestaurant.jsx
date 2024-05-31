408e028fda62a3c7da70174e8129c1b211e2173c
meri saynatkari <meri.saynatkari@gmail.com>
2024-05-31 09:09:25 +0300

    update restaurant registration layout

--- src/components/RegisterRestaurant.jsx
+++ src/components/RegisterRestaurant.jsx
@@ -1,15 +1,18 @@
-import { Text, Pressable, View, TextInput } from 'react-native';
-import { Link, useNavigate } from '../Router';
-import { useFormik } from 'formik';
-import axios from 'axios';
-import { deleteSession } from '../controllers/sessionController';
-import { restaurantValidationSchema }
-    from '../utils/formValidationSchemas';
 import { useState, useEffect } from 'react';
-import { stylesForm } from '../styles/styles';
+import axios from 'axios';
+import { useFormik } from 'formik';
+import { Text, View, ScrollView } from 'react-native';
+
+import { Link, useNavigate } from '../Router';
+import { deleteSession } from '../controllers/sessionController';
 import apiUrl from '../utils/apiUrl';
 
-const styles = stylesForm;
+import { restaurantValidationSchema }
+    from '../utils/formValidationSchemas';
+
+import createStyles from '../styles/layout';
+import { Button } from './ui/Buttons';
+import { Input, PasswordInput } from './ui/InputFields';
 
 const initialValues = {
     username: '',
@@ -51,73 +54,82 @@ const RegisterForm = ({ onSubmit, onSuccess, onError }) => {
         },
     });
 
+    const styles = createStyles();
+
     return (
-        <View style={styles.container}>
-            {formError ? (
-                <Text style={styles.error}>{formError}</Text>
-            ) : null}
-            <TextInput
-                style={styles.input}
-                placeholder='name of the restaurant'
-                value={formik.values.restaurantName}
-                onChangeText={formik.handleChange('restaurantName')}
-                onBlur={formik.handleBlur('restaurantName')}
-            />
-            {formik.touched.restaurantName && formik.errors.restaurantName && (
-                <Text style={styles.error}>{formik.errors.restaurantName}</Text>
-            )}
-            <TextInput
-                style={styles.input}
-                placeholder='username'
-                value={formik.values.username}
-                onChangeText={formik.handleChange('username')}
-                onBlur={formik.handleBlur('username')}
-            />
-            {formik.touched.username && formik.errors.username && (
-                <Text style={styles.error}>{formik.errors.username}</Text>
-            )}
-            <TextInput
-                style={styles.input}
-                placeholder='email'
-                value={formik.values.email}
-                onChangeText={formik.handleChange('email')}
-                onBlur={formik.handleBlur('email')}
-            />
-            {formik.touched.email && formik.errors.email && (
-                <Text style={styles.error}>{formik.errors.email}</Text>
-            )}
-            <TextInput
-                style={styles.input}
-                placeholder='password'
-                value={formik.values.password}
-                onChangeText={formik.handleChange('password')}
-                onBlur={formik.handleBlur('password')}
-                secureTextEntry
-            />
-            {formik.touched.password && formik.errors.password && (
-                <Text style={styles.error}>{formik.errors.password}</Text>
-            )}
-            <TextInput
-                style={styles.input}
-                placeholder='confirm password'
-                value={formik.values.confirmPassword}
-                onChangeText={formik.handleChange('confirmPassword')}
-                onBlur={formik.handleBlur('confirmPassword')}
-                secureTextEntry
-            />
-            {formik.touched.confirmPassword && formik.errors.confirmPassword &&
+        <ScrollView style={styles.background}>
+            <View style={styles.container}>
+                {formError ? (
+                    <Text style={styles.error}>{formError}</Text>
+                ) : null}
+                <Input
+                    styles={styles}
+                    placeholder='name of the restaurant'
+                    value={formik.values.restaurantName}
+                    onChangeText={formik.handleChange('restaurantName')}
+                    onBlur={formik.handleBlur('restaurantName')}
+                />
+                {formik.touched.restaurantName &&
+                formik.errors.restaurantName &&
+                (
+                    <Text style={styles.error}>
+                        {formik.errors.restaurantName}
+                    </Text>
+                )}
+                <Input
+                    styles={styles}
+                    placeholder='username'
+                    value={formik.values.username}
+                    onChangeText={formik.handleChange('username')}
+                    onBlur={formik.handleBlur('username')}
+                />
+                {formik.touched.username && formik.errors.username && (
+                    <Text style={styles.error}>{formik.errors.username}</Text>
+                )}
+                <Input
+                    styles={styles}
+                    placeholder='email'
+                    value={formik.values.email}
+                    onChangeText={formik.handleChange('email')}
+                    onBlur={formik.handleBlur('email')}
+                />
+                {formik.touched.email && formik.errors.email && (
+                    <Text style={styles.error}>{formik.errors.email}</Text>
+                )}
+                <PasswordInput
+                    styles={styles}
+                    placeholder='password'
+                    value={formik.values.password}
+                    onChangeText={formik.handleChange('password')}
+                    onBlur={formik.handleBlur('password')}
+                />
+                {formik.touched.password && formik.errors.password && (
+                    <Text style={styles.error}>{formik.errors.password}</Text>
+                )}
+                <PasswordInput
+                    styles={styles}
+                    placeholder='confirm password'
+                    value={formik.values.confirmPassword}
+                    onChangeText={formik.handleChange('confirmPassword')}
+                    onBlur={formik.handleBlur('confirmPassword')}
+                />
+                {formik.touched.confirmPassword &&
+                formik.errors.confirmPassword &&
                 <Text style={styles.error}>
                     {formik.errors.confirmPassword}
                 </Text>
-            }
-            <Pressable style={styles.button} onPress={formik.handleSubmit}>
-                <Text style={styles.buttonText}>register</Text>
-            </Pressable>
-            <Text>already registered?</Text>
-            <Link to='/login'>
-                <Text>Login</Text>
-            </Link>
-        </View>
+                }
+                <Button
+                    styles={styles}
+                    onPress={formik.handleSubmit}
+                    text='register'
+                />
+                <Text style={styles.body}>already registered?</Text>
+                <Link to='/login'>
+                    <Text style={styles.link}>login</Text>
+                </Link>
+            </View>
+        </ScrollView>
     );
 };
 
