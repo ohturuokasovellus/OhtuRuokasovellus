9540d4ac335d082bf4bd14785d730218a0a6f35a
meri saynatkari <meri.saynatkari@gmail.com>
2024-05-31 18:46:24 +0300

    fix import

--- src/__tests__/QRGenerator.test.js
+++ src/__tests__/QRGenerator.test.js
@@ -1,4 +1,4 @@
-import QRGenerator from '../components/QRGenerator';
+import QRGenerator from '../utils/QRGenerator';
 
 describe('QRGenerator', () => {
     test('QRGenerator returns a qr', () => {
