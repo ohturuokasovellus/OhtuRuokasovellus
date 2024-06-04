f74cc61b2037ee02fe6dddfef639116bb7ebfb64
meri saynatkari <meri.saynatkari@gmail.com>
2024-06-04 12:12:10 +0300

    move language switch to ui components

--- src/components/ui/LanguageSwitch.jsx
+++ src/components/ui/LanguageSwitch.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { NavButton } from './ui/Buttons';
+import { NavButton } from './Buttons';
 
 const LanguageSwitch = ({ styles }) => {
     const { i18n } = useTranslation();
