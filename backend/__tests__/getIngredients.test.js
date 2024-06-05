77d61f043bd98a303a224de37e061c4d5b4a1304
Roberto Martinez <robomarti@outlook.com>
2024-06-05 14:22:40 +0300

    added test for getIngredient()

---
+++ backend/__tests__/getIngredients.test.js
@@ -0,0 +1,30 @@
+const {getIngredients} = require('../services/getIngredients');
+const filesystem = require('fs');
+
+const data = 'id,tuoteryhmä,name,energia. laskennallinen (kJ),'+
+    'rasva (g),rasvahapot tyydyttyneet (g),hiilihydraatti imeytyvä (g),'+
+    'sokerit (g),kuitu. kokonais- (g),proteiini (g),suola (mg),'+
+    'CO2 (g/100g tuotetta)\n'+
+    '1,potatoes,potato,50,1,0.5,10,1,5,0.1,0.1,5\n'+
+    '2,fishes,fried fish,150,4,2,0,0,0,20,600,10\n';
+
+
+describe('get ingredients', () => {
+    beforeEach(() => {
+        filesystem.writeFile('backend/csvFiles/example_nutrients.csv', 
+            data, (err) => {
+            // In case of a error throw err.
+                if (err) throw err;
+            });
+    });
+
+    test('ingredients are returned correctly', async () => {
+        const ingredients = await getIngredients(
+            'backend/csvFiles/example_nutrients.csv');
+
+        const correctIngredients = {'potatoes': ['potato'], 
+            'fishes': ['fried fish']};
+
+        expect(ingredients).toEqual(correctIngredients);
+    });
+});
