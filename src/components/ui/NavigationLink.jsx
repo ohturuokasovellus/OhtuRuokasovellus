8edffce5697d4cded51b93f90c88dc42a21e6c13
meri saynatkari <meri.saynatkari@gmail.com>
2024-06-06 11:58:28 +0300

    add jsdoc for navlink

--- src/components/ui/NavigationLink.jsx
+++ src/components/ui/NavigationLink.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Link, useLocation } from '../../Router';
 
+/** Component for navigation bar links.
+ * @param {string} path path where the link leads to
+ * @param {string} text link name displayed on the nav bar
+ * @param {Object} styles styles passed from the global stylesheet
+ * @param {string} id preferably in the form 'navigation-[name]'
+ * @returns navigation link
+ */
 // eslint-disable-next-line id-length
 const NavLink = ({ path, text, styles, id }) => {
     const location = useLocation();
