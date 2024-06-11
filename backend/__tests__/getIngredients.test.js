d96b72c6f98082f9ac984058a995f398ee0ad845
Roberto Martinez <robomarti@outlook.com>
2024-06-11 09:48:16 +0300

    fixed tests

--- backend/__tests__/getIngredients.test.js
+++ backend/__tests__/getIngredients.test.js
@@ -5,9 +5,9 @@ const filesystem = require('fs');
 const data = 'id,tuoteryhmä,name,energia. laskennallinen (kJ),'+
     'rasva (g),rasvahapot tyydyttyneet (g),hiilihydraatti imeytyvä (g),'+
     'sokerit (g),kuitu. kokonais- (g),proteiini (g),suola (mg),'+
-    'CO2 (g/100g tuotetta)\n'+
-    '1,vihannekset,potato,50,1,0.5,10,1,5,0.1,0.1,5\n'+
-    '2,fishes,fried fish,150,4,2,0,0,0,20,600,10\n';
+    'CO2 (g/100g tuotetta), kasvis\n'+
+    '1,vihannekset,potato,50,1,0.5,10,1,5,0.1,0.1,5, true\n'+
+    '2,fishes,fried fish,150,4,2,0,0,0,20,600,10, false\n';
 
 
 describe('get ingredients', () => {
