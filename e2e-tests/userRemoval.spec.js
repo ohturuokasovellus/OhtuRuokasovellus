4e0e10f512212be9b21bfb51c84dac6afbabbd88
Pyry Lahtinen <pyry@pyry.info>
2024-06-12 11:07:20 +0300

    fix e2e test

--- e2e-tests/userRemoval.spec.js
+++ e2e-tests/userRemoval.spec.js
@@ -9,15 +9,20 @@ const initDb = async () => {
     await sql`TRUNCATE TABLE purchases RESTART IDENTITY CASCADE`;
     await sql`TRUNCATE TABLE restaurants RESTART IDENTITY CASCADE`;
     await insertUser('test', hash('Test123!'), 'tester@example.com', null);
+    // database already contains a removed user
+    await sql`
+        INSERT INTO users (username, password, email)
+        VALUES (NULL, NULL, NULL);
+    `;
     const restaurantId = await insertRestaurant('Good restaurant');
-    await sql`INSERT INTO meals
-        (name, restaurant_id, purchase_code, meal_description, co2_emissions,
+    await sql`INSERT INTO meals (
+        name, restaurant_id, purchase_code, meal_description, co2_emissions,
         meal_allergens, carbohydrates, protein, fat, fiber, sugar, salt,
-        saturated_fat, energy, mass, vegetable_percent)
+        saturated_fat, energy, vegetable_percent, price)
         VALUES ('Kana bolognese', ${restaurantId}, '12345678',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit,
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 50,
-        'gluteeni, selleri', 1.3, 11.7, 8.2, 0.1, 0.1, 654.7, 1.9, 523, 50, 0)`;
+        'gluteeni, selleri', 1.3, 11.7, 8.2, 0.1, 0.1, 654.7, 1.9, 523, 0, 1230)`;
 };
 
 /** @param {import('@playwright/test').Page} page */
