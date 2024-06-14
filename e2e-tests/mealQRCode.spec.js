467d761a9b5493047fa6226f139e577fd7227781
Roberto Martinez <robomarti@outlook.com>
2024-06-14 11:04:51 +0300

    fixed e2e test

--- e2e-tests/mealQRCode.spec.js
+++ e2e-tests/mealQRCode.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import {
     sql, insertUser,
-    insertRestaurant,
+    insertRestaurant, updateUserRestaurantByEmail
 } from '../backend/database';
 import { hash } from '../backend/services/hash';
 
@@ -11,7 +11,20 @@ const initTestDB = async () => {
     await sql`TRUNCATE TABLE restaurants RESTART IDENTITY CASCADE`;
     await sql`TRUNCATE TABLE meals RESTART IDENTITY CASCADE`;
 
-    const restaurantId = await insertRestaurant('testaurant');
+    const restaurant = 'testaurant';
+    const user = 'test';
+    const password = hash('Test123!');
+    const email = 'test@test.com';
+    const birthYear = '2000';
+    const gender = 'other';
+    const education = 'primary';
+    const income = 'below 1500';
+    await insertUser(user, password, email, birthYear,
+        gender, education, income
+    );
+    const restaurantId = await insertRestaurant(restaurant);
+
+    await updateUserRestaurantByEmail('test@test.com', restaurantId);
 
     await sql`INSERT INTO meals (
         name, restaurant_id, purchase_code, meal_description, co2_emissions,
@@ -24,21 +37,6 @@ const initTestDB = async () => {
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         50, 'gluteeni, selleri', 1.3, 11.7, 8.2, 0.1, 
         0.1, 654.7, 1.9, 523, 0, 1230)`;
-
-    const users = [
-        {
-            username: 'test',
-            password: 'Test123!',
-            email: 'test@test.com',
-            restaurantId: 1
-        }
-    ];
-
-    for (const user of users) {
-        const password = hash(user.password);
-        await insertUser(user.username, password, 
-            user.email, user.restaurantId);
-    }
 };
 
 test.describe('menu qr page', () => {
