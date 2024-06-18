809c1e3de421f98927b5431c787e325f5cc64243
Roberto Martinez Keisala <robomarti@outlook.com>
2024-06-18 20:59:12 +0300

    added chart for comparing restaurant meal emissions

--- src/components/RestaurantComparison.jsx
+++ src/components/RestaurantComparison.jsx
@@ -1,24 +1,117 @@
-import { useEffect,} from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { View, ScrollView, Text } from 'react-native';
+import { BarChart } from 'react-native-chart-kit';
 import { useTranslation } from 'react-i18next';
-import { View, ScrollView } from 'react-native';
+import { getSession } from '../controllers/sessionController';
 import createStyles from '../styles/styles';
+import apiUrl from '../utils/apiUrl';
+
+const chartConfig = {
+    backgroundGradientFrom: '#1E2923',
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: '#08130D',
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    barPercentage: 1
+};
 
 const RestaurantComparison = () => {
-    const {t} = useTranslation();
     const styles = createStyles();
+    const [restaurantName, setrestaurantName] = useState('');
+    const [error, setError] = useState(null);
+    const [allEmissions, setAllEmissions] = useState(0);
+    const [ownEmissions, setOwnEmissions] = useState(0);
+    const userSession = getSession();
+    const {t} = useTranslation();
+
+    if (!userSession.restaurantId){
+        return (
+            <ScrollView style={styles.background}>
+                <View style={styles.container}>
+                    <Text style={styles.body}>
+                        {t('UNAUTHORIZED')}
+                    </Text>
+                </View>
+            </ScrollView>
+        );
+    }
+
+    const restaurantId = userSession.restaurantId;
 
     useEffect(() => {
+        const getRestaurantName = async () => {
+            let response = null;
+            try {
+                response = await axios.get(
+                    `${apiUrl}/restaurant-name/${restaurantId}`);
+                setrestaurantName(response.data[0].restaurant_name);
+            } catch (error) {
+                setError(t('UNEXPECTED_ERROR'));
+            }
+        };
+
+        const getMealEmissions = async () => {
+            let response = null;
+            try {
+                response = await axios.get(
+                    `${apiUrl}/all-meal-emissions/${restaurantId}`);
+                let index = 0;
+                while (index < response.data.length) {
+                    let meal = response.data[index];
+                    setAllEmissions(previousValue => 
+                        previousValue + Number(meal.co2_emissions));
+                    if (meal.restaurant_id == restaurantId) {
+                        setOwnEmissions(previousValue => 
+                            previousValue + Number(meal.co2_emissions));
+                    }
+                    index += 1;
+                }
+            } catch (error) {
+                setError(t('UNEXPECTED_ERROR'));
+            }
+        };
+        
+        getRestaurantName();
+        getMealEmissions();
     }, []);
+
+    if (error) {
+        return (
+            <View >
+                <Text style={styles.error}>{error}</Text>
+            </View>
+        );
+    }
+
+    const chartData = {
+        labels: [restaurantName, 'All restaurants'],
+        datasets: [
+            {
+                data: [ownEmissions, allEmissions]
+            }
+        ]
+    };
 
     return (
         <ScrollView style={styles.background}>
             <View style={styles.container}>
-                <View style={styles.qrPage}>
-                </View>
+                <Text style={styles.body}>
+                    {restaurantName}
+                </Text>
+                <BarChart
+                    style={{}}
+                    data={chartData}
+                    width={500}
+                    height={220}
+                    yAxisLabel="CO2 "
+                    chartConfig={chartConfig}
+                    showValuesOnTopOfBars={true}
+                    fromZero={true}
+                />
             </View>
         </ScrollView>
     );
-
 };
 
 export default RestaurantComparison;
