a927095303d02375dcaab6210c498de904653a89
Pyry Lahtinen <pyry@pyry.info>
2024-06-18 09:40:10 +0300

    fix checkbox ui

--- src/components/ui/Checkbox.jsx
+++ src/components/ui/Checkbox.jsx
@@ -1,15 +1,27 @@
 import { Pressable, Text, View } from 'react-native';
-import { CheckBox } from 'react-native-elements';
+import { useContext } from 'react';
+import { CheckBox } from '@rneui/themed';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { themeContext } from '../../controllers/themeController';
 
 /** Styled wrapper for checkboxes. */
 const Checkbox = ({ styles, ...props}) => {
+    const { colors } = useContext(themeContext);
+    const color = colors.outline;
+
     return (
         <CheckBox
             Component={Pressable}
             containerStyle={styles.checkboxContainer}
             textStyle={styles.checkboxText}
-            checkedColor={styles.checkedIcon.backgroundColor}
-            uncheckedColor={styles.checkboxIcon.borderColor}
+            checkedIcon={
+                <MaterialCommunityIcons
+                    name='checkbox-marked' size={24} color={color} />
+            }
+            uncheckedIcon={
+                <MaterialCommunityIcons
+                    name='checkbox-blank-outline' size={24} color={color} />
+            }
             {...props}
         />
     );
@@ -19,14 +31,23 @@ const Checkbox = ({ styles, ...props}) => {
  * Only the box itself is pressable, not the title.
 */
 const CheckboxVariant = ({ styles, title, ...props}) => {
+    const { colors } = useContext(themeContext);
+    const color = colors.outline;
+
     return (
         <View style={styles.checkboxWrapper}>
             <CheckBox
                 Component={Pressable}
                 containerStyle={styles.checkboxContainerVariant}
                 textStyle={styles.checkboxTextVariant}
-                checkedColor={styles.checkedIcon.backgroundColor}
-                uncheckedColor={styles.checkboxIcon.borderColor}
+                checkedIcon={
+                    <MaterialCommunityIcons
+                        name='checkbox-marked' size={24} color={color} />
+                }
+                uncheckedIcon={
+                    <MaterialCommunityIcons
+                        name='checkbox-blank-outline' size={24} color={color} />
+                }
                 {...props}
             />
             <Text style={styles.body}>{title}</Text>
