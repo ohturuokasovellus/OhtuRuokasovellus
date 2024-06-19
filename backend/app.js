2e1e70060bf934c292720157f027bd3d9aca3897
Roberto Martinez Keisala <robomarti@outlook.com>
2024-06-19 16:48:55 +0300

    added authorization

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const pageURLRouter = require('./routes/webpageURL');
 const settingsRouter = require('./routes/settings');
 const devopsRouter = require('./routes/devops');
 const restaurantRouter = require('./routes/restaurant');
+const authorizationRouter = require('./routes/authorization');
 const path = require('path');
 const filesystem = require('fs');
 
@@ -30,6 +31,7 @@ app.use(pageURLRouter);
 app.use(settingsRouter);
 app.use(devopsRouter);
 app.use(restaurantRouter);
+app.use(authorizationRouter);
 
 const assetsPath = path.join(__dirname, 'assets');
 app.use(express.static(assetsPath, { index: false }));
