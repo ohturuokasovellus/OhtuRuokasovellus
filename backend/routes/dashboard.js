f0a237f08cf8e83d0654c30002a6459ebb1259c8
meri saynatkari <meri.saynatkari@gmail.com>
2024-06-21 12:25:37 +0300

    add error messages

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -15,6 +15,7 @@ const {
     getAvgMacronutrientsByGender,
     getAvgMacronutrientsByAgeGroup
 } = require('../databaseUtils/averages');
+const { ErrorMessage } = require('formik');
 
 // /**
 //  * Route for requesting data for dashboard.
@@ -43,7 +44,9 @@ router.get('/api/user/dashboard', async (req, res) => {
         birthYear = await getBirthYear(userInfo.userId);
         gender = await getGender(userInfo.userId);
     } catch (err) {
-        return res.sendStatus(500);
+        return res.status(500).send(
+            'error fetching birthyear/gender'
+        );
     }
 
     const age = getAgeGroup(birthYear);
@@ -53,25 +56,33 @@ router.get('/api/user/dashboard', async (req, res) => {
     try {
         averages.all.co2 = await getAvgCo2Emissions();
     } catch (err) {
-        return res.sendStatus(500);
+        return res.status(500).send(
+            'error fetching avg co2 emissions'
+        );
     }
 
     try {
         averages.user.co2 = await getAvgCo2EmissionsByUser(userInfo.userId);
     } catch (err) {
-        return res.sendStatus(500);
+        return res.status(500).send(
+            'error fetching avg co2 emissions by user'
+        );
     }
 
     try {
         averages.gender.co2 = await getAvgCo2EmissionsByGender(gender);
     } catch (err) {
-        return res.sendStatus(500);
+        return res.status(500).send(
+            'error fetching avg co2 emissions by gender'
+        );
     }
 
     try {
         averages.age.co2 = await getAvgCo2EmissionsByAgeGroup(years);
     } catch (err) {
-        return res.sendStatus(500);
+        return res.status(500).send(
+            'error fetching avg co2 emissions by age group'
+        );
     }
 
     try {
@@ -80,7 +91,9 @@ router.get('/api/user/dashboard', async (req, res) => {
         averages.all.fat = macros.avgFat;
         averages.all.protein = macros.avgProtein;
     } catch (err) {
-        return res.sendStatus(500);
+        return res.status(500).send(
+            'error fetching avg macros'
+        );
     }
 
     try {
@@ -89,7 +102,9 @@ router.get('/api/user/dashboard', async (req, res) => {
         averages.user.fat = macros.avgFat;
         averages.user.protein = macros.avgProtein;
     } catch (err) {
-        return res.sendStatus(500);
+        return res.status(500).send(
+            'error fetching avg macros by user'
+        );
     }
 
     try {
@@ -98,7 +113,9 @@ router.get('/api/user/dashboard', async (req, res) => {
         averages.gender.fat = macros.avgFat;
         averages.gender.protein = macros.avgProtein;
     } catch (err) {
-        return res.sendStatus(500);
+        return res.status(500).send(
+            'error fetching avg macros by gender'
+        );
     }
 
     try {
@@ -107,7 +124,9 @@ router.get('/api/user/dashboard', async (req, res) => {
         averages.age.fat = macros.avgFat;
         averages.age.protein = macros.avgProtein;
     } catch (err) {
-        return res.sendStatus(500);
+        return res.status(500).send(
+            'error fetching avg macros by age group'
+        );
     }
 
     res.json({
