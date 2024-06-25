3e75604094da4c9ee39188d63b6b8ab24956d86d
Roberto Martinez <robomarti@outlook.com>
2024-06-25 13:55:18 +0300

    removed language detection package since it only works on web
    didnt replace it with react native package since its pretty useless feature for now

--- src/lang/i18n.js
+++ src/lang/i18n.js
@@ -1,6 +1,5 @@
 import i18next from 'i18next';
 import {initReactI18next} from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import engl from './en.json';
 import finn from './fi.json';
 
@@ -9,18 +8,8 @@ const resources = {
     fin: finn,
 };
 
-const languageDetector = new LanguageDetector(null, {
-    order: ['localStorage', 'navigator', 'cookie', 'queryString'],
-
-    lookupLocalStorage: 'i18nextLng',
-    lookupCookie: 'i18next',
-
-    caches: ['localStorage', 'cookie']
-});
-
 i18next
     // pass the i18n instance to react-i18next.
-    .use(languageDetector)
     .use(initReactI18next)
     // init i18next
     // for all options read:
@@ -28,15 +17,11 @@ i18next
     .init({
         compatibilityJSON: 'v3',
         resources,
-        fallbackLng: 'fin',
-        // lng: 'eng', // default language to use.
-        // supportedLngs: ['fin', 'eng'],
-        detection: {
-            order: ['localStorage', 'navigator', 'cookie', 'queryString'],
-            caches: ['localStorage', 'cookie']
-        },
-        // preload: ['fin', 'eng'],
-        // initImmediate: false
+        fallbackLng: 'eng',
+        lng: 'fin', // default language to use.
+        supportedLngs: ['fin', 'eng'],
+        preload: ['fin', 'eng'],
+        initImmediate: false
     });
 
 export default {i18next};
